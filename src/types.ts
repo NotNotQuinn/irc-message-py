ccef57588fae915d4da37e0d23e382f981855f03
Quinn T <Quinn.github@gmail.com>
2021-01-08 19:45:52 -0600

    remove ts files

--- src/types.ts
+++
@@ -1,9 +0,0 @@
-export interface IRCMessage {
-  command: string | null;
-  prefix: string | null;
-  tags: Record<string, string>;
-  params: string[];
-  raw: string;
-  param: string;
-  trailing: string;
-}
